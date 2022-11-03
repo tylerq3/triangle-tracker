e8b304fbb491657ddb7f86912f0c4410a4f0e3f0
tyler quinn <tyler.quinn032@gmail.com>
2022-11-02 20:45:42 -0700

    add js, css, img, format html
    co-authored by: Hydaly Ouanesavanh <ouanehyd000@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+function hideResults() {
+  document.getElementById("Side1").setAttribute("class", "hidden");
+  document.getElementById("Side2").setAttribute("class", "hidden");
+  document.getElementById("Side3").setAttribute("class", "hidden");
+}
+
+function add(num1, num2, num3) {
+  return num1 + num2 <= num3;
+}
+
+//UI Logic//
+function handleCalculation(event) {
+  event.preventDefault();
+  const Side1 = parseInt(document.querySelector("input#Side1").value)
+  const Side2 = parseInt(document.querySelector("input#Side2").value)
+  const Side3 = parseInt(document.querySelector("input#Side3").value)
+
+  let result;
+  if (Side1 === Side2 === Side3) {
+    document.getElementById("Equilateral").removeAttribute("class");
+    result = "Equilateral";
+  } else if (Side1 === Side2 || Side2 === Side3 || Side1 === Side3)
+
+
+
+  document.getElementById("output").innerText = result; 
+}
+
+window.addEventListener("load", function() {
+  const form = document.getElementById("Triangles");
+  form.addEventListener("submit", handleCalculation);
+});
